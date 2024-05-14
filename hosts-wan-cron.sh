cd7022542cfec6e763a98143ba235a47bcd2ce51
klcb2010 <32628414+klcb2010@users.noreply.github.com>
2024-05-14 21:18:51 +0800

    Update hosts-wan-cron.sh

--- hosts-wan-cron.sh
+++ hosts-wan-cron.sh
@@ -3,11 +3,9 @@
 # 定义目录和文件URL  
 BASE_URL="https://mirror.ghproxy.com/raw.githubusercontent.com/klcb2010/restart_wan-and-hosts-mod/main/"  
 DEST_DIR_SCRIPTS="/jffs/scripts/"  
-DEST_DIR_CRONTABS="$DEST_DIR_SCRIPTS/crontabs/"  
   
 # 创建目标目录（如果不存在）  
 mkdir -p "$DEST_DIR_SCRIPTS"  
-mkdir -p "$DEST_DIR_CRONTABS"  
   
 # 下载文件并记录日志  
 function download_file {  
@@ -20,15 +18,15 @@ function download_file {
     echo "Downloaded $url to $dest"  
 }  
   
-download_file "$BASE_URL/copy_hosts.sh" "$DEST_DIR_SCRIPTS/copy_hosts.sh"  
-download_file "$BASE_URL/restart_wan.sh" "$DEST_DIR_SCRIPTS/restart_wan.sh"  
-download_file "$BASE_URL/set_crontab.sh" "$DEST_DIR_SCRIPTS/set_crontab.sh"  
-download_file "$BASE_URL/klcb2010" "$DEST_DIR_CRONTABS/klcb2010"  
-  
 # 赋予执行权限给shell脚本文件  
-chmod +x "$DEST_DIR_SCRIPTS/copy_hosts.sh"  
-chmod +x "$DEST_DIR_SCRIPTS/restart_wan.sh"  
-chmod +x "$DEST_DIR_SCRIPTS/set_crontab.sh"  
+function chmod_script {  
+    local script=$1  
+    chmod +x "$script" || {  
+        echo "Error: Failed to set executable permission for $script"  
+        exit 1  
+    }  
+    echo "Set executable permission for $script"  
+}  
   
 # 执行脚本并记录日志  
 function execute_script {  
@@ -41,10 +39,21 @@ function execute_script {
     echo "$script executed successfully."  
 }  
   
+# 下载文件  
+download_file "$BASE_URL/copy_hosts.sh" "$DEST_DIR_SCRIPTS/copy_hosts.sh"  
+download_file "$BASE_URL/restart_wan.sh" "$DEST_DIR_SCRIPTS/restart_wan.sh"  
+download_file "$BASE_URL/set_crontab.sh" "$DEST_DIR_SCRIPTS/set_crontab.sh"  
+  
+# 赋予执行权限  
+chmod_script "$DEST_DIR_SCRIPTS/copy_hosts.sh"  
+chmod_script "$DEST_DIR_SCRIPTS/restart_wan.sh"  
+chmod_script "$DEST_DIR_SCRIPTS/set_crontab.sh"  
+  
+# 执行脚本  
 execute_script "$DEST_DIR_SCRIPTS/copy_hosts.sh"  
   
-# 重启WAN连接的脚本，请谨慎使用  
-# execute_script "$DEST_DIR_SCRIPTS/restart_wan.sh"  # 取消注释以执行  
+# 注意：重启WAN连接是一个敏感操作，请确保您了解其影响后再执行  
+# execute_script "$DEST_DIR_SCRIPTS/restart_wan.sh"  
   
 execute_script "$DEST_DIR_SCRIPTS/set_crontab.sh"  
   
